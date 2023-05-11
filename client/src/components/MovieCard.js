db0f3041011a5be0dc036f1acdb118d40148bbd6
Georgi Damyanov <damyanov95@gmail.com>
2023-05-11 21:49:55 +0200

    update movie card

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -5,6 +5,38 @@ import '../App.css';
 const MovieCard = (props) => {
     const movie = props.movie;
 
+    const stars_rating_1 = () => {
+        let stars = [];
+
+        for (let i = 1; i <= movie.rating_1; i++) {
+            stars.push(
+                <img
+                    style={{ width: '25px' }}
+                    alt='star'
+                    src='https://www.freepnglogos.com/uploads/star-png/stars-png-images-star-clipart-images-icons-37.png'
+                />
+            );
+        }
+
+        return stars;
+    };
+
+    const stars_rating_2 = () => {
+        let stars = [];
+
+        for (let i = 1; i <= movie.rating_2; i++) {
+            stars.push(
+                <img
+                    style={{ width: '25px' }}
+                    alt='star'
+                    src='https://www.freepnglogos.com/uploads/star-png/stars-png-images-star-clipart-images-icons-37.png'
+                />
+            );
+        }
+
+        return stars;
+    };
+
     return (
         <div className='card-container'>
             <img
@@ -16,8 +48,10 @@ const MovieCard = (props) => {
                     <Link to={`/show-movie/${movie._id}`}>{movie.title}</Link>
                 </h2>
                 <p>{movie.release_date}</p>
-                <p>Eli's rating: 1/{movie.rating_1}</p>
-                <p>Georgi's rating: 1/{movie.rating_2}</p>
+                <p>Eli's rating:</p>
+                <p>{stars_rating_1()}</p>
+                <p>Georgi's rating:</p>
+                <p>{stars_rating_2()}</p>
             </div>
         </div>
     );
