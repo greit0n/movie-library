1284c13c93a363e9e11c0979d709ae516ac85bab
Eliswer <106431426+Eliswer@users.noreply.github.com>
2023-05-16 15:48:35 +0200

    ++

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -1,64 +1,64 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+import "./movieCard.css";
 
 const MovieCard = (props) => {
-    const movie = props.movie;
+  const movie = props.movie;
 
-    const stars_rating_1 = () => {
-        let stars = [];
+  const stars_rating_1 = () => {
+    let stars = [];
 
-        for (let i = 1; i <= movie.rating_1; i++) {
-            stars.push(
-                <img
-                    style={{ width: '25px' }}
-                    alt='star'
-                    src='https://www.freepnglogos.com/uploads/star-png/stars-png-images-star-clipart-images-icons-37.png'
-                    key={i}
-                />
-            );
-        }
+    for (let i = 1; i <= movie.rating_1; i++) {
+      stars.push(
+        <img
+          style={{ width: "25px" }}
+          alt="star"
+          src="https://www.freepnglogos.com/uploads/star-png/stars-png-images-star-clipart-images-icons-37.png"
+          key={i}
+        />
+      );
+    }
 
-        return stars;
-    };
+    return stars;
+  };
 
-    const stars_rating_2 = () => {
-        let stars = [];
+  const stars_rating_2 = () => {
+    let stars = [];
 
-        for (let i = 1; i <= movie.rating_2; i++) {
-            stars.push(
-                <img
-                    style={{ width: '25px' }}
-                    alt='star'
-                    src='https://www.freepnglogos.com/uploads/star-png/stars-png-images-star-clipart-images-icons-37.png'
-                    key={i}
-                />
-            );
-        }
+    for (let i = 1; i <= movie.rating_2; i++) {
+      stars.push(
+        <img
+          style={{ width: "25px" }}
+          alt="star"
+          src="https://www.freepnglogos.com/uploads/star-png/stars-png-images-star-clipart-images-icons-37.png"
+          key={i}
+        />
+      );
+    }
 
-        return stars;
-    };
+    return stars;
+  };
 
-    return (
-        <div className='card-container'>
-            <img
-                src={movie.poster_path}
-                alt='Movies'
-            />
-            <div className='desc'>
-                <h2>
-                    <Link to={`/show-movie/${movie._id}`}>
-                        {movie.title ?? movie.name}
-                    </Link>
-                </h2>
-                <p>{movie.release_date ?? movie.first_air_date}</p>
-                <p>Eli's rating:</p>
-                <p>{stars_rating_1()}</p>
-                <p>Georgi's rating:</p>
-                <p>{stars_rating_2()}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="card-container">
+      <Link to={`/show-movie/${movie._id}`}>
+        <img src={movie.poster_path} alt="Movies" />
+      </Link>
+      <div className="desc">
+        <h2>
+          <Link to={`/show-movie/${movie._id}`}>
+            {movie.title ?? movie.name}
+          </Link>
+        </h2>
+        <p>{movie.release_date ?? movie.first_air_date}</p>
+        <p>Eli's rating:</p>
+        <p className="stars">{stars_rating_1()}</p>
+        <p>Georgi's rating:</p>
+        <p className="stars">{stars_rating_2()}</p>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
--- client/src/components/ShowMovieList.js
+++ client/src/components/ShowMovieList.js
@@ -1,134 +1,146 @@
-import React, { useState, useEffect } from 'react';
-import '../App.css';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import MovieCard from './MovieCard';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import MovieCard from "./MovieCard";
+import "./showMovieList.css";
 
 function ShowMovieList() {
-    const [movies, setMovies] = useState([]);
-    const [searchMovies, setSearchMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [searchMovies, setSearchMovies] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    axios
+      .get("http://localhost:8082/api/movies")
+      .then((res) => {
+        setMovies(res.data);
+      })
+      .catch((err) => {
+        console.log("Error from ShowMovieList");
+      });
+  }, []);
+
+  const movieList =
+    movies.length === 0
+      ? "there is no movie record!"
+      : movies.map((movie, k) => <MovieCard movie={movie} key={k} />);
+
+  const onChange = (e) => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/search/multi?api_key=056e734e28201f2ab1e4b8369b0b5bb7&query=" +
+          e.target.value
+      )
+      .then((res) => {
+        setSearchMovies(res?.data.results);
+      })
+      .catch((err) => {
+        console.log("Error from ShowMovieList");
+      });
+  };
+
+  const addMovie = (movie) => {
+    let searchMovie = { ...movie };
+    searchMovie.poster_path =
+      "https://image.tmdb.org/t/p/w200/" + searchMovie.poster_path;
+
+    console.log(searchMovie);
+
+    axios
+      .post("http://localhost:8082/api/movies", searchMovie)
+      .then((res) => {
+        // Push to /
         axios
-            .get('http://localhost:8082/api/movies')
-            .then((res) => {
-                setMovies(res.data);
-            })
-            .catch((err) => {
-                console.log('Error from ShowMovieList');
-            });
-    }, []);
+          .get("http://localhost:8082/api/movies")
+          .then((res) => {
+            setMovies(res.data);
+          })
+          .catch((err) => {
+            console.log("Error from ShowMovieList");
+          });
+      })
+      .catch((err) => {
+        console.log("Error in CreateMovie!");
+      });
+  };
 
-    const movieList =
-        movies.length === 0
-            ? 'there is no movie record!'
-            : movies.map((movie, k) => (
-                  <MovieCard
-                      movie={movie}
-                      key={k}
-                  />
-              ));
+  const handleBlur = (e) => {
+    e.target.value = "";
+    /* axios
+      .get(
+        "https://api.themoviedb.org/3/search/multi?api_key=056e734e28201f2ab1e4b8369b0b5bb7&query=" +
+          e.target.value
+      )
+      .then((res) => {
+        setSearchMovies(res?.data.results);
+      })
+      .catch((err) => {
+        console.log("Error from ShowMovieList");
+      });*/
+  };
 
-    const onChange = (e) => {
-        axios
-            .get(
-                'https://api.themoviedb.org/3/search/multi?api_key=056e734e28201f2ab1e4b8369b0b5bb7&query=' +
-                    e.target.value
-            )
-            .then((res) => {
-                setSearchMovies(res?.data.results);
-            })
-            .catch((err) => {
-                console.log('Error from ShowMovieList');
-            });
-    };
+  return (
+    <div className="ShowMovieList">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <br />
+            <h2 className="display-4 text-center">Movie List</h2>
+          </div>
 
-    const addMovie = (movie) => {
-        let searchMovie = { ...movie };
-        searchMovie.poster_path =
-            'https://image.tmdb.org/t/p/w200/' + searchMovie.poster_path;
+          <div className={"search-wrapper col-md-12"}>
+            <label htmlFor="search" className="search">
+              Search
+            </label>
 
-        console.log(searchMovie);
+            <input
+              className="input"
+              name="search"
+              type="text"
+              onChange={onChange}
+              onBlur={handleBlur}
+            ></input>
+          </div>
 
-        axios
-            .post('http://localhost:8082/api/movies', searchMovie)
-            .then((res) => {
-                // Push to /
-                axios
-                    .get('http://localhost:8082/api/movies')
-                    .then((res) => {
-                        setMovies(res.data);
-                    })
-                    .catch((err) => {
-                        console.log('Error from ShowMovieList');
-                    });
-            })
-            .catch((err) => {
-                console.log('Error in CreatMovie!');
-            });
-    };
-
-    return (
-        <div className='ShowMovieList'>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-md-12'>
-                        <br />
-                        <h2 className='display-4 text-center'>Movie List</h2>
-                    </div>
-
-                    <div className='col-md-12'>
-                        <label htmlFor='search'>Search</label>
-
-                        <input
-                            name='search'
-                            type='text'
-                            onChange={onChange}
-                        ></input>
-                    </div>
-
-                    <div className='search-movie-container'>
-                        {searchMovies.map((item, i) => {
-                            return (
-                                <div
-                                    className='search-movie'
-                                    key={item.id}
-                                >
-                                    <img
-                                        style={{ width: '100px' }}
-                                        src={
-                                            'https://image.tmdb.org/t/p/w200/' +
-                                            item.poster_path
-                                        }
-                                        alt='img'
-                                        onClick={() => {
-                                            addMovie(item);
-                                        }}
-                                    />
-                                    <span>{item.title}</span>
-                                </div>
-                            );
-                        })}
-                    </div>
-
-                    <div className='col-md-11'>
-                        <Link
-                            to='/create-movie'
-                            className='btn btn-outline-warning float-right'
-                        >
-                            + Add New Movie
-                        </Link>
-                        <br />
-                        <br />
-                        <hr />
-                    </div>
+          <div className="search-movie-container">
+            {searchMovies.map((item, i) => {
+              return (
+                <div className="search-movie" key={item.id}>
+                  <div className="img-wrapper">
+                    <img
+                      style={{ width: "100px" }}
+                      src={
+                        "https://image.tmdb.org/t/p/w200/" + item.poster_path
+                      }
+                      alt="img"
+                      onClick={() => {
+                        addMovie(item);
+                      }}
+                    />
+                  </div>
+                  <span>{item.title}</span>
                 </div>
+              );
+            })}
+          </div>
 
-                <div className='list'>{movieList}</div>
-            </div>
+          <div className="col-md-11">
+            <Link
+              to="/create-movie"
+              className="btn btn-outline-warning float-right"
+            >
+              + Add New Movie
+            </Link>
+            <br />
+            <br />
+            <hr />
+          </div>
         </div>
-    );
+
+        <div className="list">{movieList}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ShowMovieList;
